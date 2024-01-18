e3bcab60ff380c4a4b616acd8e0a017a608eb614
ngeorges <ngeorges.dev@gmail.com>
2024-01-18 22:32:46 +0100

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  icons: {
+    icon: "/icon.ico",
+  },
   title: "Modular UI",
   description:
     "Craft beautiful, accessible, and responsive Flutter UIs with a Modular-UI Design-inspired component library",
