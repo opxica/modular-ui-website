47fc9aed327cac97997167edb2c14a57e2b311cb
ngeorges <ngeorges.dev@gmail.com>
2024-01-18 22:37:21 +0100

    refactor config tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,14 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'header-pattern': "url('/bg_01.png')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
-export default config
+};
+export default config;
