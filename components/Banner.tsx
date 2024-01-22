feb5403c78534589ecfcbf994794a9f6f8b0c17a
ngeorges <ngeorges.dev@gmail.com>
2024-01-22 20:18:54 +0100

    rafactor name theme switcher

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -39,7 +39,7 @@ const Banner = () => {
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <Link
-                className="text-md flex h-12 w-32 items-center justify-center space-x-2 rounded-full border border-neutral-800 bg-gray-100 bg-neutral-800/50 font-sf-pro-rounded-regular text-neutral-100 shadow-xl transition-all hover:border-neutral-500/10 hover:text-neutral-100/75 hover:shadow-neutral-700/10"
+                className="text-md flex w-36 items-center justify-center space-x-2 rounded-full border border-neutral-800 bg-gray-100 bg-neutral-800/50 py-2.5 font-sf-pro-rounded-regular text-neutral-100 shadow-xl transition-all hover:border-neutral-500/10 hover:text-neutral-100/75 hover:shadow-neutral-700/10"
                 href="https://pub.dev/packages/modular_ui"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -54,15 +54,15 @@ const Banner = () => {
               </Link>
 
               <Link
-                className="group relative flex h-12 w-36 items-center justify-center overflow-hidden rounded-full border border-neutral-800/50 bg-transparent text-center font-sf-pro-rounded-regular text-sm text-neutral-100 transition hover:border-neutral-800/85 hover:text-neutral-100/85"
+                className="group relative flex w-36 items-center justify-center overflow-hidden rounded-full border border-neutral-800/50 bg-transparent py-3 text-center font-sf-pro-rounded-regular text-sm text-neutral-100 transition hover:border-neutral-800/85 hover:text-neutral-100/85"
                 href="https://github.com/opxica/modular-ui-website"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <span className="absolute -end-full transition-all group-hover:end-4">
-                  <IconStarFilled size={14} className="text-yellow-200" />
+                  <IconStarFilled size={14} className="text-yellow-500" />
                 </span>
-                <div className="flex items-center gap-1">
+                <div className="flex items-center space-x-1">
                   <IconBrandGithub size={14} />
                   <span className="transition-all group-hover:me-4">
                     Github Star
